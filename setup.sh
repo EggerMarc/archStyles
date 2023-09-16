9887b04100eb2464dbe634096acb332813d60fad
EggerMarc <eggermarc@gmail.com>
2023-09-16 17:38:01 -0300

    setup and gitignore

--- setup.sh
+++ setup.sh
@@ -11,17 +11,21 @@ fi
 if $download; then
     cd "images/"|| exit;
     echo "Downloading Dataset...";
-    gallery-dl --range 1-500 https://www.pinterest.it/AnaUtrillaInteriorismo/minimal-interior-design-interiores-minimalistas/;
-    gallery-dl --range 1-500 https://www.pinterest.it/historiasdecasa/sala-de-estar-living-room/;
-    gallery-dl --range 1-500 https://www.pinterest.it/casavoguebrasil/sala-de-estar/;
-    gallery-dl --range 1-500 https://www.pinterest.it/luxxu/living-room-decoration/;
-    gallery-dl --range 1-500 https://www.pinterest.it/eggermarc/interior-design/;
-    gallery-dl --range 1-500 https://www.pinterest.it/Designeddecor/living-room-decor/;
-    gallery-dl --range 1-500 https://www.pinterest.it/decoratedlifer/living-room-decor/;
-    gallery-dl --range 1-500 https://www.pinterest.it/susanlori/living-room-decor/;
+    gallery-dl --range 1-500 https://www.pinterest.it/AnaUtrillaInteriorismo/minimal-interior-design-interiores-minimalistas/ &
+    gallery-dl --range 1-500 https://www.pinterest.it/historiasdecasa/sala-de-estar-living-room/ &
+    gallery-dl --range 1-500 https://www.pinterest.it/casavoguebrasil/sala-de-estar/ &
+    gallery-dl --range 1-500 https://www.pinterest.it/luxxu/living-room-decoration/ &
+    gallery-dl --range 1-500 https://www.pinterest.it/eggermarc/interior-design/ &
+    gallery-dl --range 1-500 https://www.pinterest.it/Designeddecor/living-room-decor/ &
+    gallery-dl --range 1-500 https://www.pinterest.it/decoratedlifer/living-room-decor/ &
+    gallery-dl --range 1-500 https://www.pinterest.it/susanlori/living-room-decor/ &
+    wait;
     find . -mindepth 2 -type f -print -exec mv {} . \;
     find . -type d -empty -delete;
     find . -type f ! -iname "*.jpg" ! -iname "*.png" -delete
     ls -v | cat -n | while read n f; do mv -n "$f" "$n.jpg"; done;
-    cd "..";
+    cd "../src";
+    python3 cleaner.py;
+    ls -v | cat -n | while read n f; do mv -n "$f" "$n.jpg"; done;
+    cd ".."; 
 fi
